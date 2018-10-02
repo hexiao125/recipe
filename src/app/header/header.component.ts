56a92795818989020545105a1ac26a613dae27a0
Xiao <hexiao125@gmail.com>
2018-10-02 11:36:30 +0200

    logout and some route protection

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Response } from '@angular/http';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
 
   onSaveRecipe() {
     this.dataStorageService.storeRecipes().subscribe(
@@ -19,6 +20,10 @@ export class HeaderComponent {
 
   onFetchRecipe() {
     this.dataStorageService.getRecipes();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 
 }
